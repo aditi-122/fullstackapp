1226a7f672918b3c2adccf62c447243eaea6b237
aditi pathak <https://github.com/26komalagrawal>
2024-12-26 15:38:50 +0530

    quiz app

---
+++ frontEnd/Script/nabar.js
@@ -0,0 +1,7 @@
+const navbar = () => {
+    const nav = `<div class="cont">
+        <a href="./Script/index.js">Home</a>
+        <a href="./Script/quiz.html">Quiz</a>
+        <a href="./Script/question.html">Question</a>
+    </div>`
+};
